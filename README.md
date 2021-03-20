38374f4d1fb06fe41227966ff0c08cd1b60edc78
Josemar Lohn <j@lo.hn>
2021-03-20 07:55:56 +0000

    new: add main functions

--- README.md
+++ README.md
@@ -1,2 +1,68 @@
 # dcurl
-Distributed cURL
+
+Distributed cURL command line
+
+Heavily based on the *-fetch code created by [RangerMauve](https://github.com/RangerMauve)
+for their outstanding project [Agregore Browser](https://github.com/AgregoreWeb/agregore-browser)
+**you should try**
+
+## Install
+
+`npm install -g dcurl`
+
+or
+
+`yarn global add dcurl`
+
+
+## Use
+
+In this early versions you can only use GET requests and the output is always the terminal.
+
+```
+dcurl protocol://server/directory/filename
+```
+This output will be sent to the screen
+If you want to save to a file or want to get a binary file, like an image, you can use:
+
+```
+dcurl protocol://server/directory/filename > newfilename
+```
+
+
+### Dat Protocol
+
+Apparently the DAT protocol support doesn't run with old DAT drives.
+I have to test this more.
+
+```
+dcurl dat://blog.maeve.moe
+```
+
+
+### Gemini Protocol
+
+```
+dcurl gemini://gemini.circumlunar.space/
+```
+
+```
+dcurl gemini://hellomouse.net/mouse.jpg  > mouse.jpg
+```
+
+
+### Hypercore protocol
+
+```
+dcurl hyper://blog.maeve.moe/
+```
+
+
+### IPFS protocol
+
+The support for IPNS is still broken. Only IPNS requests can be made.
+
+```
+dcurl ipns://ipfs.io/
+```
+
---
+++ bin.js
@@ -0,0 +1,139 @@
+#!/usr/bin/env node
+'use strict'
+const pkg = require('./package.json')
+const input = process.argv.slice(2)
+const parseCurl = require('./parse-curl')
+const eosp = require('end-of-stream-promise')
+const { Readable } = require('streamx')
+var parsed
+
+const helpString =
+    `${pkg.description}
+Usage
+  $ dcurl <curl_command>
+  
+Options
+  -v, --version	Print version
+  -h, --help	Display help message`
+
+function printHelp () {
+    console.log(helpString)
+    process.exit(0)
+}
+
+function printVersion () {
+    console.log(pkg.version)
+    process.exit(0)
+}
+
+function printError (n = 0) {
+    console.error('I don\'t understand the parameters. Error: ' + n)
+    process.exit(-1)
+}
+
+function toFetch (data) {
+
+    parsed = parseCurl(data)
+
+    return `
+      fetch( 
+            '${parsed.url}', 
+            {headers:${JSON.stringify(parsed.header)},
+            method:'${parsed.method}'}
+           )
+           .then(console.log, console.error)`
+}
+
+function init (data) {
+    let res = toFetch(data)
+    if (parsed.url)
+        switch (parsed.protocol) {
+            case 'dat':
+                (async function () {
+                    const fetch = require('dat-fetch')()
+                    try {
+                        const response = await fetch(parsed.url)
+                        const stream = Readable.from(response.body)
+                        stream.pipe(process.stdout)
+                        await eosp(stream)
+                    } finally {
+                        process.exit()
+                    }
+                })()
+                break;
+            case 'gemini':
+                (async function () {
+                    const fetch = require('gemini-fetch')()
+                    try {
+                        const response = await fetch(parsed.url)
+                        const stream = Readable.from(response.body)
+                        stream.pipe(process.stdout)
+                        await eosp(stream)
+                    } finally {
+                        process.exit()
+                    }
+                })()
+                break;
+            case 'hyper':
+                (async function () {
+                    const fetch = require('hypercore-fetch')()
+                    try {
+                        const response = await fetch(parsed.url)
+                        const stream = Readable.from(response.body)
+                        stream.pipe(process.stdout)
+                        await eosp(stream)
+                    } finally {
+                        fetch.close()
+                    }
+                })()
+                break;
+            case 'ipfs':
+            case 'ipns':
+                (async function () {
+                    const IPFS = require('ipfs')
+                    const makeIpfsFetch = require('js-ipfs-fetch')
+                    const ipfs = await IPFS.create({ silent: true })
+                    const fetch = await makeIpfsFetch({ ipfs })
+                    try {
+                        const response = await fetch(parsed.url)
+                        const stream = Readable.from(response.body)
+                        stream.pipe(process.stdout)
+                        await eosp(stream)
+                    } finally {
+                        process.exit()
+                    }
+                })()
+                break;
+            default:
+                console.log('Protocol ' + parsed.protocol + ' not supported');
+                process.exit(-1);
+        }
+
+
+
+
+
+
+    else
+        printError(1)
+}
+
+process.argv.map(arg => {
+    switch (arg) {
+        case '-h':
+        case '--help':
+            printHelp()
+            break
+        case '-v':
+        case '--version':
+            printVersion()
+    }
+})
+
+if (process.stdin.isTTY)
+    if (!input)
+        printHelp()
+    else
+        init(input)
+else
+    process.stdin.once('data', data => init(data))
